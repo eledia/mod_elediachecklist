ddd1ce570e01d621c7e25ad498d2a23f25340c3c
ngeiges <ng@eledia.de>
2022-08-30 15:28:08 +0200

    Die Termin-PDF-Strings sind jetzt in  gespeichert

--- mod/elediachecklist/export_termin_pdf.php
+++ mod/elediachecklist/export_termin_pdf.php
@@ -80,120 +80,47 @@ function text_of_id($elediachecklist_item_id) {
 
         // 01 = (C ->  3) Beschreibung der Prüfungskonfiguration
         case 3:
-            $txt  = '';
-            $txt .= '<b>Beschreibung der Prüfungskonfiguration</b><br /><br />';
-            $txt .= 'Teilen Sie uns bitte die gewünschte Prüfungskonfiguration mit. Z.B.';
-            $txt .= '<ul style="">';
-            $txt .= '<li>Sie benötigen einen PDF-Reader</li>';
-            $txt .= '<li>Sie möchten den Studierenden am Bildschirm einen kleinen Taschenrechner zur Verfügung stellen.</li>';
-            $txt .= '<li>Sie möchten, dass Ihre Studierenden während der Klausur eine weitere Anwendung nutzen.</li>';
-            $txt .= '</ul>';
-            $txt .= 'Hinweis! Je früher Sie uns Ihren Konfigurationswunsch mitteilen, desto größer ist die Chance, dass wir diesen umsetzen können.';
+            $txt = get_string('text_pdf_01', 'elediachecklist');
             break;
 
         // 02 = (E ->  5) Fertigstellung und Funktionstest der Klausur
         case 5:
-            $txt  = '';
-            $txt .= '<b>Fertigstellung und Funktionstest der Klausur</b><br /><br />';
-            $txt .= 'Prüfen Sie Ihre Fragen auf folgende Eigenschaften:';
-            $txt .= '<ul style="">';
-            $txt .= '<li>Die Summe der Punkte bei einer Aufgabe darf kein negativer Wert möglich sein. Dies ist prüfungsrechtlich nicht zulässig.</li>';
-            $txt .= '<li>In Word erstellte Textbausteine wurden vor dem Einfügen in eine Frage oder ein Assessment von dem in Word automatisch vorhandenen Quellcode befreit. Der Quellcode könnte die Funktion der Fragen unerwartet beeinflussen.</li>';
-            $txt .= '<li>Alle Fragen wurden auf eine korrekte Punkteberechnung hin geprüft.</li>';
-            $txt .= '</ul>';
-            $txt .= 'Bevor Sie Ihre Klausur zur Qualitätskontrolle dem E-Klausur-Team bereitstellen, ist ein interner Funktionstest obligatorisch, um Fehler oder fehlende Funktionalitäten frühzeitig zu erkennen.';
-            $txt .= '<ul style="">';
-            $txt .= '<li>Generieren Sie ein "richtiges Prüfungsergebnis". Eine Anleitung finden Sie in den Schulungsunterlagen auf dem Prüfungssystem.</li>';
-            $txt .= '<li>Bei <b>Zufallsauswahl</b> von Fragen ist zusätzlich eine Testklausur mit allen Fragen zu erstellen und zu erproben.</li>';
-            $txt .= '<li>Wenn Sie beim Funktionstest Unterstützung wünschen, wenden Sie sich bitte an das E-Klausur-Team.</li>';
-            $txt .= '</ul>';
-            $txt .= 'Bei der Endabnahme bestätigen Sie, dass Sie die o. g. Punkte geprüft haben.';
+            $txt = get_string('text_pdf_02', 'elediachecklist');
             break;
 
         // 03 = (F ->  6) Bereitstellung der Klausur für Anpassungen und Qualitätskontrolle
         case 6:
-            $txt  = '';
-            $txt .= '<b>Bereitstellung der Klausur für Anpassungen und Qualitätskontrolle</b><br /><br />';
-            $txt .= 'An jeder Klausur nehmen wir einige manuelle Anpassungen vor:<br />';
-            $txt .= 'Aktivierung einer manipulationssicheren Prüfungsumgebung, Sicherheitseinstellung für den Fall einer Prüfungsunterbrechung u.a.m.<br />';
-            $txt .= 'Außerdem überprüfen wir Ihre Klausur hinsichtlich der Funktionalität. ';
-            $txt .= 'Informieren Sie uns kurz über das Ankündigungsforum Ihres Prüfungskurses oder per E-Mail über die Bereitstellung der Klausur. ';
-            $txt .= 'Wir geben Ihnen gerne eine Rückmeldung auf die prüfungsdidaktische Gestaltung Ihrer Klausur. Wenn Sie prüfungsdidaktisch von uns beraten werden möchten, lassen Sie uns dies bitte frühzeitig wissen. ';
+            $txt = get_string('text_pdf_03', 'elediachecklist');
             break;
 
         // 04 = (G ->  7) Endabnahme E-Klausur
         case 7:
-            $txt  = '';
-            $txt .= '<b>Endabnahme E-Klausur</b><br /><br />';
-            $txt .= 'Im Beisein des/der Lehrenden oder eines/r autorisierten Vertreters:in wird die Klausur unter Prüfungsbedingungen getestet. ';
-            $txt .= 'Im Vordergrund steht der Funktionstest Ihrer Klausur in der originären Prüfungsumgebung nach dem Vier-Augen-Prinzip:';
-            $txt .= '<ul style="">';
-            $txt .= '<li>Alle "kritischen" Einstellungen werden überprüft.</li>';
-            $txt .= '<li>Bei Zufallsauswahl von Fragen wird eine Testklausur mit allen Fragen erprobt.</li>';
-            $txt .= '<li>Die korrekte Archiverstellung wird überprüft.</li>';
-            $txt .= '<li>Die Funktion der manuellen Bewertung von Freitextfragen wird überprüft.</li>';
-            $txt .= '<li>Der Ergebnisexport wird überprüft.</li>';
-            $txt .= '</ul>';
-            $txt .= 'Es wird ein Prüfprotokoll erstellt und von den Beteiligten der Endabnahme unterschrieben. ';
-            $txt .= 'Setzen Sie sich bitte zur detaillierten Terminabsprache mit uns in Verbindung!';
+            $txt = get_string('text_pdf_04', 'elediachecklist');
             break;
 
         // 05 = (H ->  8) HIS-Liste an E-Klausurteam senden
         case 8:
-            $txt  = '';
-            $txt .= '<b>HIS-Liste an E-Klausurteam senden</b><br /><br />';
-            $txt .= 'Wir benötigen von Ihnen bzw. Ihrem Sekretariat eine Teilnehmerliste der - in HIS - angemeldeten Prüflinge im Excel-Format. ';
-            $txt .= 'Lassen Sie uns über das Ankündigungsforum Ihres Prüfungskurses eine Nachricht mit der Liste als Anhang zukommen oder senden Sie uns eine E-Mail.';
-            $txt .= '<ul style="">';
-            $txt .= '<li>Diese Liste sollte mindestens die Namen, Vornamen und Matrikelnummern der Studierenden enthalten.</li>';
-            $txt .= '<li>Personen mit Nachteilsausgleich / Zugehörige der COVID-19-Risikogruppe sollten in der Liste gekennzeichnet sein.</li>';
-            $txt .= '<li>Wir ergänzen die Listen mit Passwörtern und erstellen Etiketten mit den Zugangsdaten, die jede/r Teilnehmer:in der Klausur am Eingang zum E-Assessmentcenter erhält.</li>';
-            $txt .= '</ul>';
+            $txt = get_string('text_pdf_05', 'elediachecklist');
             break;
 
         // 06 = (I ->  9) Gruppeneinteilung mit Namen und Matrikelnummer
         case 9:
-            $txt  = '';
-            $txt .= '<b>Gruppeneinteilung mit Namen und Matrikelnummer</b><br /><br />';
-            $txt .= 'Bitte beachten Sie: Unter Corona-Bedingungen erstellt das E-Klausurteam eine eventuell erforderliche Gruppeneinteilung und stimmt diese mit Ihnen ab.';
+            $txt = get_string('text_pdf_06', 'elediachecklist');
             break;
 
         // 07 = (J -> 10) Namen und Anzahl der Aufsichtspersonen, Studierendeninformationen
         case 10:
-            $txt  = '';
-            $txt .= '<b>Namen und Anzahl der Aufsichtspersonen, Studierendeninformationen</b><br /><br />';
-            $txt .= 'Bitte gewährleisten Sie, dass von Ihrem Fachgebiet mindestens eine Person pro Klausurraum zur Klausuraufsicht ';
-            $txt .= 'vor Ort ist, die eine fachliche Aufsicht gewährleisten kann (studentische Hilfskräfte oder Mitarbeiter:innen ';
-            $txt .= 'aus Sekretariaten können diese Aufsicht personell verstärken). Diese Person ist bitte 30 Minuten vor Einlass ';
-            $txt .= 'der Studierenden im E-Assessmentcenter. Beachten Sie bitte Folgendes: Sollte ein Student/eine Studentin ';
-            $txt .= 'in einem separaten Raum z.B. im E-Assessmentcenter schreiben, ist für diese Person eine eigene Aufsichtsperson ';
-            $txt .= 'erforderlich. Teilen Sie uns bitte bis zum angegebenen Zeitpunkt Namen und E-Mail- Adresse aller eingeplanten ';
-            $txt .= 'Aufsichtspersonen mit.<br />';
-            $txt .= 'Informieren Sie Ihre Studierenden, dass diese mit Studierenden- und Lichtbildausweis pünktlich zur ';
-            $txt .= 'Einlasszeit am E-Assessmentcenter erscheinen. Folgende umfassende Informationen zu E-Klausuren finden ';
-            $txt .= 'Ihre Studierenden auf der Website unikassel.de/go/eklausur-studis:';
-            $txt .= '<ul style="">';
-            $txt .= '<li>Eine Wegbeschreibung zum E-Assessmentcenter und ggf. anderer genutzter Räumlichkeiten (z.B. in der Kurt-Wolters-Str. 5)</li>';
-            $txt .= '<li>Das Video "Einführung für Studierende" zum Ablauf und zur Durchführung einer Klausur</li>';
-            $txt .= '<li>Die Möglichkeit des Schreibens einer Probeklausur, um die Prüfungsumgebung kennenzulernen</li>';
-            $txt .= '</ul>';
+            $txt = get_string('text_pdf_07', 'elediachecklist');
             break;
 
         // 08 = (M -> 15) Mitteilung an E-Klausurteam: Klausureinsicht und Korrekturen abgeschlossen
         case 15:
-            $txt  = '';
-            $txt .= '<b>Mitteilung an E-Klausurteam: Klausureinsicht und Korrekturen abgeschlossen</b><br /><br />';
-            $txt .= 'Teilen Sie uns mit, wenn Sie<br />';
-            $txt .= '(1) Freitextfragen manuell bewertet haben. Wir stellen Ihnen dann aktualisierte PDF-Dokumente für die Klausureinsicht zur Verfügung.<br />';
-            $txt .= '(2) Nachkorrekturen an den automatisierten Bewertungen vorgenommen haben. Wir stellen Ihnen dann aktualisierte PDF-Dokumente für die Klausureinsicht zur Verfügung.<br />';
-            $txt .= '(3) die Klausureinsicht abgeschlossen haben. Sollte es in Folge der Klausureinsicht zu Bewertungsänderungen kommen, teilen Sie uns dies bitte ebenfalls mit, damit wie diese Änderungen abschließend archivieren können.';
+            $txt = get_string('text_pdf_08', 'elediachecklist');
             break;
 
         // 09 = (O -> 17) Notenschlüssel bereitgestellt
         case 17:
-            $txt  = '';
-            $txt .= '<b>Notenschlüssel bereitgestellt</b><br /><br />';
-            $txt .= 'Senden Sie uns Ihren Notenschlüssel zu. Diesen müssen mir zusammen mit Ihrer Klausur archivieren.';
+            $txt = get_string('text_pdf_09', 'elediachecklist');
             break;
 
         default:
--- mod/elediachecklist/lang/en/elediachecklist.php
+++ mod/elediachecklist/lang/en/elediachecklist.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Strings for component 'eledia', language 'en'
+ * Strings for component elediachecklist, language 'en'
  *
  * @package   mod_elediachecklist
  * @copyright 2021 Davo Smith
@@ -306,3 +306,87 @@ $string['viewsingleupdate'] = 'Update progress for this user';
 
 $string['yesnooverride'] = 'Yes, cannot override';
 $string['yesoverride'] = 'Yes, can override';
+
+
+$string['text_pdf_01'] = '<b>Beschreibung der Prüfungskonfiguration</b><br /><br />'
+.'Teilen Sie uns bitte die gewünschte Prüfungskonfiguration mit. Z.B.'
+.'<ul style="">'
+.'<li>Sie benötigen einen PDF-Reader</li>'
+.'<li>Sie möchten den Studierenden am Bildschirm einen kleinen Taschenrechner zur Verfügung stellen.</li>'
+.'<li>Sie möchten, dass Ihre Studierenden während der Klausur eine weitere Anwendung nutzen.</li>'
+.'</ul>'
+.'Hinweis! Je früher Sie uns Ihren Konfigurationswunsch mitteilen, desto größer ist die Chance, dass wir diesen umsetzen können.';
+
+$string['text_pdf_02'] = '<b>Fertigstellung und Funktionstest der Klausur</b><br /><br />'
+.'Prüfen Sie Ihre Fragen auf folgende Eigenschaften:'
+.'<ul style="">'
+.'<li>Die Summe der Punkte bei einer Aufgabe darf kein negativer Wert möglich sein. Dies ist prüfungsrechtlich nicht zulässig.</li>'
+.'<li>In Word erstellte Textbausteine wurden vor dem Einfügen in eine Frage oder ein Assessment von dem in Word automatisch vorhandenen Quellcode befreit. Der Quellcode könnte die Funktion der Fragen unerwartet beeinflussen.</li>'
+.'<li>Alle Fragen wurden auf eine korrekte Punkteberechnung hin geprüft.</li>'
+.'</ul>'
+.'Bevor Sie Ihre Klausur zur Qualitätskontrolle dem E-Klausur-Team bereitstellen, ist ein interner Funktionstest obligatorisch, um Fehler oder fehlende Funktionalitäten frühzeitig zu erkennen.'
+.'<ul style="">'
+.'<li>Generieren Sie ein "richtiges Prüfungsergebnis". Eine Anleitung finden Sie in den Schulungsunterlagen auf dem Prüfungssystem.</li>'
+.'<li>Bei <b>Zufallsauswahl</b> von Fragen ist zusätzlich eine Testklausur mit allen Fragen zu erstellen und zu erproben.</li>'
+.'<li>Wenn Sie beim Funktionstest Unterstützung wünschen, wenden Sie sich bitte an das E-Klausur-Team.</li>'
+.'</ul>'
+.'Bei der Endabnahme bestätigen Sie, dass Sie die o. g. Punkte geprüft haben.';
+
+$string['text_pdf_03'] = '<b>Bereitstellung der Klausur für Anpassungen und Qualitätskontrolle</b><br /><br />'
+.'An jeder Klausur nehmen wir einige manuelle Anpassungen vor:<br />'
+.'Aktivierung einer manipulationssicheren Prüfungsumgebung, Sicherheitseinstellung für den Fall einer Prüfungsunterbrechung u.a.m.<br />'
+.'Außerdem überprüfen wir Ihre Klausur hinsichtlich der Funktionalität. '
+.'Informieren Sie uns kurz über das Ankündigungsforum Ihres Prüfungskurses oder per E-Mail über die Bereitstellung der Klausur. '
+.'Wir geben Ihnen gerne eine Rückmeldung auf die prüfungsdidaktische Gestaltung Ihrer Klausur. Wenn Sie prüfungsdidaktisch von uns beraten werden möchten, lassen Sie uns dies bitte frühzeitig wissen. ';
+
+$string['text_pdf_04'] = '<b>Endabnahme E-Klausur</b><br /><br />'
+.'Im Beisein des/der Lehrenden oder eines/r autorisierten Vertreters:in wird die Klausur unter Prüfungsbedingungen getestet. '
+.'Im Vordergrund steht der Funktionstest Ihrer Klausur in der originären Prüfungsumgebung nach dem Vier-Augen-Prinzip:'
+.'<ul style="">'
+.'<li>Alle "kritischen" Einstellungen werden überprüft.</li>'
+.'<li>Bei Zufallsauswahl von Fragen wird eine Testklausur mit allen Fragen erprobt.</li>'
+.'<li>Die korrekte Archiverstellung wird überprüft.</li>'
+.'<li>Die Funktion der manuellen Bewertung von Freitextfragen wird überprüft.</li>'
+.'<li>Der Ergebnisexport wird überprüft.</li>'
+.'</ul>'
+.'Es wird ein Prüfprotokoll erstellt und von den Beteiligten der Endabnahme unterschrieben. '
+.'Setzen Sie sich bitte zur detaillierten Terminabsprache mit uns in Verbindung!';
+
+$string['text_pdf_05'] = '<b>HIS-Liste an E-Klausurteam senden</b><br /><br />'
+.'Wir benötigen von Ihnen bzw. Ihrem Sekretariat eine Teilnehmerliste der - in HIS - angemeldeten Prüflinge im Excel-Format. '
+.'Lassen Sie uns über das Ankündigungsforum Ihres Prüfungskurses eine Nachricht mit der Liste als Anhang zukommen oder senden Sie uns eine E-Mail.'
+.'<ul style="">'
+.'<li>Diese Liste sollte mindestens die Namen, Vornamen und Matrikelnummern der Studierenden enthalten.</li>'
+.'<li>Personen mit Nachteilsausgleich / Zugehörige der COVID-19-Risikogruppe sollten in der Liste gekennzeichnet sein.</li>'
+.'<li>Wir ergänzen die Listen mit Passwörtern und erstellen Etiketten mit den Zugangsdaten, die jede/r Teilnehmer:in der Klausur am Eingang zum E-Assessmentcenter erhält.</li>'
+.'</ul>';
+
+$string['text_pdf_06'] = '<b>Gruppeneinteilung mit Namen und Matrikelnummer</b><br /><br />'
+.'Bitte beachten Sie: Unter Corona-Bedingungen erstellt das E-Klausurteam eine eventuell erforderliche Gruppeneinteilung und stimmt diese mit Ihnen ab.';
+
+$string['text_pdf_07'] = '<b>Namen und Anzahl der Aufsichtspersonen, Studierendeninformationen</b><br /><br />'
+.'Bitte gewährleisten Sie, dass von Ihrem Fachgebiet mindestens eine Person pro Klausurraum zur Klausuraufsicht '
+.'vor Ort ist, die eine fachliche Aufsicht gewährleisten kann (studentische Hilfskräfte oder Mitarbeiter:innen '
+.'aus Sekretariaten können diese Aufsicht personell verstärken). Diese Person ist bitte 30 Minuten vor Einlass '
+.'der Studierenden im E-Assessmentcenter. Beachten Sie bitte Folgendes: Sollte ein Student/eine Studentin '
+.'in einem separaten Raum z.B. im E-Assessmentcenter schreiben, ist für diese Person eine eigene Aufsichtsperson '
+.'erforderlich. Teilen Sie uns bitte bis zum angegebenen Zeitpunkt Namen und E-Mail- Adresse aller eingeplanten '
+.'Aufsichtspersonen mit.<br />'
+.'Informieren Sie Ihre Studierenden, dass diese mit Studierenden- und Lichtbildausweis pünktlich zur '
+.'Einlasszeit am E-Assessmentcenter erscheinen. Folgende umfassende Informationen zu E-Klausuren finden '
+.'Ihre Studierenden auf der Website unikassel.de/go/eklausur-studis:'
+.'<ul style="">'
+.'<li>Eine Wegbeschreibung zum E-Assessmentcenter und ggf. anderer genutzter Räumlichkeiten (z.B. in der Kurt-Wolters-Str. 5)</li>'
+.'<li>Das Video "Einführung für Studierende" zum Ablauf und zur Durchführung einer Klausur</li>'
+.'<li>Die Möglichkeit des Schreibens einer Probeklausur, um die Prüfungsumgebung kennenzulernen</li>'
+.'</ul>';
+
+$string['text_pdf_08'] = '<b>Mitteilung an E-Klausurteam: Klausureinsicht und Korrekturen abgeschlossen</b><br /><br />'
+.'Teilen Sie uns mit, wenn Sie<br />'
+.'(1) Freitextfragen manuell bewertet haben. Wir stellen Ihnen dann aktualisierte PDF-Dokumente für die Klausureinsicht zur Verfügung.<br />'
+.'(2) Nachkorrekturen an den automatisierten Bewertungen vorgenommen haben. Wir stellen Ihnen dann aktualisierte PDF-Dokumente für die Klausureinsicht zur Verfügung.<br />'
+.'(3) die Klausureinsicht abgeschlossen haben. Sollte es in Folge der Klausureinsicht zu Bewertungsänderungen kommen, teilen Sie uns dies bitte ebenfalls mit, damit wie diese Änderungen abschließend archivieren können.';
+
+$string['text_pdf_09'] = '<b>Notenschlüssel bereitgestellt</b><br /><br />'
+.'Senden Sie uns Ihren Notenschlüssel zu. Diesen müssen mir zusammen mit Ihrer Klausur archivieren.';
+
